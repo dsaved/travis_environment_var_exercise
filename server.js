51e7d663ada7988a30d0fab5ea6863866faa3d4d
Daniel Johnson <dsaved8291@gmail.com>
2022-05-21 19:51:07 +0000

    Initial Commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log("Microservices rock!");
-    await sleep(5000);
-  }
+    while (true) {
+        const favoriteFood = process.env.FAVORITE_FOOD;
+        console.log(`My favorite food is ${favoriteFood}`);
+        await sleep(5000);
+    }
 }
 
 main();
