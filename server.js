6f4d21df4281ec3fccea14b137339d1983181552
Daniel Johnson <dsaved8291@gmail.com>
2022-05-22 08:29:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
     while (true) {
-        const favoriteFood = process.env.FAVORITE_FOOD;
         console.log(`My favorite food is ${favoriteFood}`);
         await sleep(5000);
     }
